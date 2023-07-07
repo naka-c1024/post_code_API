98eaafb3da9524a770e43035f31967c014eb8931
naka-c1024 <ny42800824@gmail.com>
2023-07-07 12:39:43 +0900

    add: 外部APIからjsonデータ取得完了

--- main.go
+++ main.go
@@ -2,14 +2,40 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World\n")
+func step1(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is step1!\n")
+}
+
+func address(w http.ResponseWriter, r *http.Request) {
+	postalCode := r.FormValue("postal_code")
+	if postalCode == "" {
+		fmt.Fprintf(w, "Enter postal_code\n")
+		return
+	}
+
+	url := "https://geoapi.heartrails.com/api/json?method=searchByPostal&postal=" + postalCode
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(w, "http.Get error: %v\n", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintf(w, "io.ReadAll error: %v\n", err)
+	}
+
+	fmt.Fprintf(w, string(body)) // 取得した情報は[]byteなのでstringに型変換
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", step1)
+	http.HandleFunc("/address", address)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
